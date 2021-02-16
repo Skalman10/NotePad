626a21428620856026564bcfca337d2a811efc4c
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2021-02-16 10:57:09 +0100

    Nu kan man öppna en textfil som skrivs ut i textarea

--- src/NotePad.java
+++ src/NotePad.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class NotePad {
     public NotePad() {
@@ -8,6 +9,37 @@ public class NotePad {
             @Override
             public void actionPerformed(ActionEvent e) {
                 textArea1.setText("");
+            }
+        });
+        MOpen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent f) {
+                JFileChooser fc = new JFileChooser();
+                int result = fc.showOpenDialog(null);
+                String filename;
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    filename = fc.getSelectedFile().getAbsolutePath();
+                } else {
+                    filename = null;
+                }
+
+                FileReader fr = null;
+                try {
+                    fr = new FileReader(filename);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                BufferedReader inFile = new BufferedReader(fr);
+
+                String line;
+                try {
+                    while ((line = inFile.readLine() ) != null) {
+                        textArea1.append(line);
+                    }
+                    inFile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
